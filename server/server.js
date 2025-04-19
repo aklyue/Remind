4917785f2ac0420d0d17c282694f842b7876cf20
aklyue <118197740+aklyue@users.noreply.github.com>
2025-04-19 15:55:23 +0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ app.use("/", authRoutes);
 app.use("/users", userRoutes);
 app.use("/upload", uploadRoutes);
 
-app.listen(PORT,() => {
+app.listen(PORT, "0.0.0.0",() => {
   console.log(`Server is running on port ${PORT}`);
 });
 
